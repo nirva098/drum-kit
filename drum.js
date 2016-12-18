57faa1d08a1491303f2a0f67ce3255124c4c97a5
Nirav Bharadiya <niravthunder@gmail.com>
2016-12-18 15:24:33 +0530

    all files added

---
+++ drum.js
@@ -0,0 +1,73 @@
+$(document).click(function () {
+	// body...
+	console.log("clicked!");
+});
+
+
+/*$(document).keypress(function (e) {
+	console.log(String.fromCharCode(e.keyCode));
+});*/
+
+/* .keypress() API of jQuery 
+
+We can add functionality when some key is pressed .
+
+We can select the desired area by selector
+
+$(document).keypress(function(){};);
+
+Also , if we want to keep track of which key is pressed , we can pass that key as argument
+Ex. 
+
+$(document).keypress(function (e) {
+	// here in this function e is the key pressed
+
+	// we can access the ASCII code of key press by following function
+	console.log(e.keyCode);
+
+	// we can access the character by , string.fromCharCode() method
+	console.log(string.fromCharCode(e.keyCode));
+});
+*/
+
+//window.addEventListener("keypress",function(e){console.log(e.keyCode)});
+
+//equivalent jQuery code 
+/*$(document).keypress(function (e) {
+	// body...
+	console.log("jQuery , key pressed : ",String.fromCharCode(e.keyCode));
+});*/
+
+/*
+	keypress : alphanumeric key (all distinct , capitals and smalls)
+	keydown : when key is pressed (A means A , no a )
+	keyup : when key is released .
+*/
+
+/*window.addEventListener("keyup",function(e){
+	console.log("Key : ",String.fromCharCode(e.keyCode)," is just released !");
+});*/
+
+/*$(document).keyup(function  (e) {
+	// body...
+	console.log("jQuery , Key released : ",String.fromCharCode(e.keyCode));
+});*/
+
+/*window.addEventListener("keydown",function(e){
+	console.log("Key : ",String.fromCharCode(e.keyCode)," is just pressed !");
+});*/
+
+/*$(document).keydown(function  (e) {
+	// body...
+	console.log("jQuery , Key pressed down : ",String.fromCharCode(e.keyCode));
+});
+*/
+
+$(document).keydown(function  (e) {
+	// body...
+	var character = String.fromCharCode(e.keyCode);
+	if(character === 'A') {
+		$("#A").style.backgroundColor = "black";
+	}
+}
+);
